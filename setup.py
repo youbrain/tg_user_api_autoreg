8502f4672faab9a063bd583ba91eb7c4d6233f43
Alexashka <647533sancho@gmail.com>
2020-06-24 21:51:56 +0300

    README.md edited, Alpha is available now

--- setup.py
+++ setup.py
@@ -1,14 +1,15 @@
 from setuptools import setup, find_packages
-
 from os import path
+
+
 this_directory = path.abspath(path.dirname(__file__))
 with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
 
 
 setup(name='tg_api_autoreg',
-      version='0.1.2',
-      description='Library for automatic registration telegram User Api (beta)',
+      version='0.1.3',
+      description='Library for automatic registration telegram User Api (Alpha)',
       install_requires=['requests'],
       author='Alexandr Macheck',
       url="https://github.com/youbrain/tg_user_api_autoreg",
